1d99762b0b4c24f855f8889668fc3c77665b523b
Joao Vitor Lima <joaovitorsocial25@gmail.com>
2022-10-25 14:48:25 -0300

    Primeiro commit

---
+++ src/main/java/com/gestao/fazenda/controleRebanho/ControleDeRebanhoApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.fazenda.controleRebanho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeRebanhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeRebanhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao/fazenda/controleRebanho/ControleDeRebanhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.fazenda.controleRebanho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeRebanhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
